df3b5f5f9eca4d45dff184cf65333b7283dd3a1d
Javi <javiera.jorquera.p@usach.cl>
2023-07-17 23:10:32 -0400

    CRUD Pipe

--- src/main/java/com/civilo/roller/services/PipeService.java
+++ src/main/java/com/civilo/roller/services/PipeService.java
@@ -4,7 +4,7 @@ import com.civilo.roller.Entities.PipeEntity;
 import com.civilo.roller.repositories.PipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.civilo.roller.exceptions.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,4 +25,43 @@ public class PipeService {
         return pipeRepository.findById(id);
     }
 
+    // Create
+    // Permite guardar un objeto del tipo "PipeEntity" en la base de datos.
+    public PipeEntity createPipe(PipeEntity pipe){   
+        return pipeRepository.save(pipe);  
+    }
+
+    // Permite verificar si existe el tipo de tubo ingresado.
+    public Optional<PipeEntity> validatePipeName(String pipeName){
+        return Optional.ofNullable(pipeRepository.findByPipeName(pipeName));  
+    }
+
+    // Update
+    // Permite actualizar los datos de un objeto del tipo "PipeEntity" en la base de datos.
+    public PipeEntity updatePipe(Long pipeID, PipeEntity pipe){
+        PipeEntity existingPipe = pipeRepository.findById(pipeID)
+            .orElseThrow(() -> new EntityNotFoundException("Pipe no encontrado con el ID: " + pipeID));
+
+        existingPipe.setPipeName(pipe.getPipeName());
+        
+        PipeEntity updatedPipe = pipeRepository.save(existingPipe);
+        return updatedPipe;
+    }
+
+    // Delete all
+    // Permite eliminar todos los tubos de un sistema.
+    public void deletePipes() {
+        pipeRepository.deleteAll();
+    }
+
+    // Delete by id
+    // Permite eliminar un tubo en especifico del sistema.
+    public void deletePipeById(Long id){
+        pipeRepository.deleteById(id);
+    }
+    
+    // Permite verificar si existe un tubo en el sistema, segun el id ingresado.
+    public boolean existsPipeById(Long id){
+        return pipeRepository.findById(id).isPresent();
+    }
 }
